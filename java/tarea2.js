a23793331c6185e87d0bd3337254de680b4748cb
AgusG <aagusgimenez@hotmail.com>
2019-10-09 08:49:01 -0300

    hola

---
+++ java/tarea2.js
@@ -0,0 +1,9 @@
+var grupo = ["Franco", "Agustina", "Federico", "Leo", "Bianca", "Ivo", "Tomás", "Gabriel", "Abelardo", "Alexis", "Walter", "Marcos", "Sebastian", "Luciano", "Sole", "Gonza"]
+
+console.log(grupo.length);
+
+console.log(grupo.sort())
+
+console.log(grupo[0])
+
+console.log(grupo[last])
